ba8ab341a460a5fa0a4afc416c8d0988b238f79e
EdBoi999 <edwardlicon50@gmail.com>
2023-02-09 03:12:16 -0500

    first object

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -14,6 +14,16 @@ const FeatureList = [
     ),
   },
   {
+    title: 'My First Object',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
     title: 'Focus on What Matters',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
